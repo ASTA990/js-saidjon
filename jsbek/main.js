c879f7f6c95c1a1736df385341d646d738faf061
ASTA990 <grandsaidjon@gmail.com>
2026-05-04 16:21:45 +0500

    zdd

---
+++ jsbek/main.js
@@ -0,0 +1,17 @@
+let talabaIsmi = "Elmurod"
+        talabaIsmi = "Mo'minjon"
+        talabaIsmi = "Shohruzbek"
+        console.log(talabaIsmi)
+
+        const yosh = 15
+        yosh = 16 //bu yerda const bo'lganligi sababi yoshni qiymati o'zgartirilmaydi
+        console.log(yosh)
+
+        let age = 16
+
+        if (age > 18){
+            console.log("Siz balog'at yoshiga yetgansiz")
+        }
+        else {
+            console.log("Siz balog'at yoshiga yetmagansiz")
+        }
