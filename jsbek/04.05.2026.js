6a9a2751cce83d2495ad49a8496111a148ddc1c3
ASTA990 <grandsaidjon@gmail.com>
2026-05-11 16:58:53 +0500

    add

---
+++ jsbek/04.05.2026.js
@@ -0,0 +1,36 @@
+let son1 = 13
+let son2 = 4
+let qoshish = son1 + son2
+console.log(qoshish)
+let ayirish = son1 - son2
+console.log(ayirish)
+let kopaytirish = son1 * son2
+console.log(kopaytirish)
+let bolish = son1 / son2
+console.log(bolish)
+let daraja = son1 ** son2
+console.log(daraja) 
+let qoldiq = son1 % son2
+console.log(qoldiq)
+
+console.log(son1++)
+console.log(son1)
+console.log(son1--)
+console.log(son1)
+
+//true-togri
+//false-notogri
+
+let a = '4'
+let b = 4
+let c = '2afsfsdf13asdas!@#$asdsa'
+let togri = true
+let notogri = false
+console.log(a == b)
+console.log(a === b)
+//ma'lumot turini tekshirish
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(togri))
+console.log(typeof(notogri))
---
+++ jsbek/11.05.2026.js
@@ -0,0 +1,13 @@
+let son = 12 
+console.log(typeof(son))
+let tekst = "12"
+console.log(typeof(tekst))
+let ozgaruvchi = "Shoxruzbek123qwe"
+console.log(typeof(ozgaruvchi))
+
+console.log(typeof(son + tekst))
+console.log(son == tekst)
+console.log(son === tekst)
+console.log(son ** 2)
+console.log(son / 3)
+console.log()
