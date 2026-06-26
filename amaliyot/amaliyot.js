89d68ad060b92398389d6ffc100b7e8f28c3c581
ASTA990 <grandsaidjon@gmail.com>
2026-06-26 17:11:25 +0500

    add

---
+++ amaliyot/amaliyot.js
@@ -0,0 +1,409 @@
+// 1-masala
+// 5 va 7 sonlarining yig'indisini toping.
+// let a1 = 5,
+//   b1 = 7;
+// Javob:
+// console.log(a1 + b1);
+
+// 2-masala
+// 12 va 4 sonlarining ayirmasini toping.
+// Javob:
+// console.log(12 - 4);
+
+// 3-masala
+// 8 va 3 sonlarining ko'paytmasini toping.
+// Javob:
+// console.log(8 * 3);
+
+// 4-masala
+// 20 ni 4 ga bo'ling.
+// Javob:
+// console.log(20 / 4);
+
+// 5-masala
+// "Salom" so'zini 3 marta konsolga chiqaring (for).
+// Javob:
+// for (let i = 1; i <= 3; i++) {
+//   console.log("Salom");
+// }
+
+// 6-masala
+// 1 dan 5 gacha sonlarni chiqaring (for).
+// Javob:
+// for (let i = 1; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// 7-masala
+// 10 dan 6 gacha sonlarni chiqaring (while).
+// Javob:
+// let i7 = 10;
+// while (i7 >= 6) {
+//   console.log(i7);
+//   i7--;
+// }
+
+// 8-masala
+// Berilgan son 10 dan katta bo'lsa "Katta", aks holda "Kichik" deb chiqaring (if/else).
+// let son8 = 15;
+// Javob:
+// if (son8 > 10) {
+//   console.log("Katta");
+// } else {
+//   console.log("Kichik");
+// }
+
+// 9-masala
+// Berilgan son 5 ga teng bo'lsa "Teng", aks holda "Teng emas" deb chiqaring.
+// let son9 = 5;
+// Javob:
+// if (son9 === 5) {
+//   console.log("Teng");
+// } else {
+//   console.log("Teng emas");
+// }
+
+// 10-masala
+// 1 dan 4 gacha sonlarning yig'indisini toping (for).
+// Javob:
+// let yigindi10 = 0;
+// for (let i = 1; i <= 4; i++) {
+//   yigindi10 += i;
+// }
+// console.log(yigindi10);
+
+// 11-masala
+// Berilgan son musbat bo'lsa "Musbat" deb chiqaring.
+// let son11 = 7;
+// Javob:
+// if (son11 > 0) {
+//   console.log("Musbat");
+// }
+
+// 12-masala
+// 2 dan 8 gacha bo'lgan juft sonlarni chiqaring (for).
+// Javob:
+// for (let i = 2; i <= 8; i += 2) {
+//   console.log(i);
+// }
+
+// 13-masala
+// 1 dan 10 gacha bo'lgan sonlarning ko'paytmasini toping (while).
+// Javob:
+// let kopaytma13 = 1;
+// let i13 = 1;
+// while (i13 <= 10) {
+//   kopaytma13 *= i13;
+//   i13++;
+// }
+// console.log(kopaytma13);
+
+// 14-masala
+// Berilgan ikkita sonning kattasini aniqlang (if/else).
+// let x14 = 12,
+//   y14 = 8;
+// Javob:
+// if (x14 > y14) {
+//   console.log(x14);
+// } else {
+//   console.log(y14);
+// }
+
+// 15-masala
+// "JavaScript" so'zidagi harflar sonini toping.
+// let soz15 = "JavaScript";
+// Javob:
+// console.log(soz15.length);
+
+// 16-masala
+// 1 dan 7 gacha toq sonlarni chiqaring (for).
+// Javob:
+// for (let i = 1; i <= 7; i += 2) {
+//   console.log(i);
+// }
+
+// 17-masala
+// Berilgan son 0 ga teng bo'lsa "Nol" deb chiqaring (if).
+// let son17 = 0;
+// Javob:
+// if (son17 === 0) {
+//   console.log("Nol");
+// }
+
+// 18-masala
+// 5 dan 1 gacha sonlarni teskari chiqaring (while).
+// Javob:
+// let i18 = 5;
+// while (i18 >= 1) {
+//   console.log(i18);
+//   i18--;
+// }
+
+// 19-masala
+// Berilgan son 100 dan kichik bo'lsa "Kichik", aks holda "Katta yoki teng" deb chiqaring.
+// let son19 = 50;
+// Javob:
+// if (son19 < 100) {
+//   console.log("Kichik");
+// } else {
+//   console.log("Katta yoki teng");
+// }
+
+// 20-masala
+// 1 dan 3 gacha sonlarni va ularning kvadratlarini chiqaring (for).
+// Javob:
+// for (let i = 1; i <= 3; i++) {
+//   console.log(i, i * i);
+// }
+
+// 21-masala
+// Foydalanuvchi yoshini tekshiring. Agar yosh 18 dan katta yoki teng bo'lsa "Voyaga yetgan", aks holda "Voyaga yetmagan" deb chiqaring.
+// let yosh21 = 20;
+// Javob:
+// if (yosh21 >= 18) {
+//   console.log("Voyaga yetgan");
+// } else {
+//   console.log("Voyaga yetmagan");
+// }
+
+// 22-masala
+// 1 dan 50 gacha bo'lgan sonlardan faqat 7 ga bo'linadiganlarini chiqaring (for + if).
+// Javob:
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 7 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 23-masala
+// Berilgan sonning raqamlar sonini toping (while). Masalan: 1234 -> 4 ta raqam.
+// let son23 = 1234;
+// Javob:
+// let count23 = 0;
+// while (son23 > 0) {
+//   count23++;
+//   son23 = Math.floor(son23 / 10);
+// }
+// console.log(count23);
+
+// 24-masala
+// Uchta sondan eng kichigini toping (if/else).
+// let a24 = 15,
+//   b24 = 8,
+//   c24 = 12;
+// Javob:
+// let min24 = a24;
+// if (b24 < min24) min24 = b24;
+// if (c24 < min24) min24 = c24;
+// console.log(min24);
+
+// 25-masala
+// 10 dan 1 gacha bo'lgan sonlarni va ularning kubini chiqaring (for).
+// Javob:
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i, i ** 3);
+// }
+
+// 26-masala
+// Berilgan sonning raqamlar yig'indisini toping (while). Masalan: 123 -> 1+2+3=6
+// let son26 = 123;
+// Javob:
+// let sum26 = 0;
+// while (son26 > 0) {
+//   sum26 += son26 % 10;
+//   son26 = Math.floor(son26 / 10);
+// }
+// console.log(sum26);
+
+// 27-masala
+// Berilgan sonning teskarisini toping (while). Masalan: 123 -> 321
+// let son27 = 123;
+// Javob:
+// let reverse27 = 0;
+// while (son27 > 0) {
+//   reverse27 = reverse27 * 10 + (son27 % 10);
+//   son27 = Math.floor(son27 / 10);
+// }
+// console.log(reverse27);
+
+// 28-masala
+// 1 dan 100 gacha bo'lgan sonlardan 3 ga va 5 ga bo'linadiganlarini chiqaring (for + if).
+// Javob:
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 29-masala
+// Berilgan sonning factorialini hisoblang (for). Masalan: 5! = 120
+// let n29 = 5;
+// Javob:
+// let fact29 = 1;
+// for (let i = 1; i <= n29; i++) {
+//   fact29 *= i;
+// }
+// console.log(fact29);
+
+// 30-masala
+// Berilgan son juft va 10 dan katta bo'lsa "Qabul", aks holda "Rad" deb chiqaring (if/else).
+// let son30 = 12;
+// Javob:
+// if (son30 % 2 === 0 && son30 > 10) {
+//   console.log("Qabul");
+// } else {
+//   console.log("Rad");
+// }
+
+// 31-masala
+// 1 dan N gacha bo'lgan toq sonlar yig'indisini toping (while).
+// let N31 = 10;
+// Javob:
+// let sum31 = 0;
+// let i31 = 1;
+// while (i31 <= N31) {
+//   if (i31 % 2 !== 0) {
+//     sum31 += i31;
+//   }
+//   i31++;
+// }
+// console.log(sum31);
+
+// 32-masala
+// Berilgan son ikkala shartni tekshiring: son musbatmi va son juftmi (if/else).
+// let son32 = 6;
+// Javob:
+// if (son32 > 0 && son32 % 2 === 0) {
+//   console.log("Musbat va juft");
+// } else {
+//   console.log("Shart bajarilmadi");
+// }
+
+// 33-masala
+// 1 dan 30 gacha bo'lgan sonlardan 4 ga bo'linadiganlarini chiqaring va ularning sonini toping (for + if).
+// Javob:
+// let count33 = 0;
+// for (let i = 1; i <= 30; i++) {
+//   if (i % 4 === 0) {
+//     console.log(i);
+//     count33++;
+//   }
+// }
+// console.log(count33);
+
+// 34-masala
+// Berilgan sonning barcha bo'luvchilarini toping (for). Masalan: 12 -> 1,2,3,4,6,12
+// let son34 = 12;
+// Javob:
+// for (let i = 1; i <= son34; i++) {
+//   if (son34 % i === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 35-masala
+// 1 dan 100 gacha bo'lgan sonlarning o'rtacha qiymatini toping (for).
+// Javob:
+// let sum35 = 0;
+// for (let i = 1; i <= 100; i++) {
+//   sum35 += i;
+// }
+// console.log(sum35 / 100);
+
+// 36-masala
+// Berilgan ikki son orasidagi barcha sonlarni chiqaring (while).
+// let start36 = 3,
+//   end36 = 8;
+// Javob:
+// while (start36 <= end36) {
+//   console.log(start36);
+//   start36++;
+// }
+
+// 37-masala
+// Berilgan sonning raqamlari ichida eng kattasini toping (while). Masalan: 382 -> 8
+// let son37 = 382;
+// Javob:
+// let max37 = 0;
+// while (son37 > 0) {
+//   let digit = son37 % 10;
+//   if (digit > max37) {
+//     max37 = digit;
+//   }
+//   son37 = Math.floor(son37 / 10);
+// }
+// console.log(max37);
+
+// 38-masala
+// 10 dan 100 gacha bo'lgan sonlardan birinchi raqami ikkinchi raqamidan kichik bo'lganlarini chiqaring (for).
+// Javob:
+// for (let i = 10; i < 100; i++) {
+//   let a = Math.floor(i / 10);
+//   let b = i % 10;
+//   if (a < b) {
+//     console.log(i);
+//   }
+// }
+
+// 39-masala
+// Berilgan son 50 dan katta va 100 dan kichik bo'lsa "Oraliqda", aks holda "Oraliqda emas" (if/else).
+// let son39 = 75;
+// Javob:
+// if (son39 > 50 && son39 < 100) {
+//   console.log("Oraliqda");
+// } else {
+//   console.log("Oraliqda emas");
+// }
+
+// 40-masala
+// 1 dan N gacha bo'lgan sonlar ichida 2 ga ham, 3 ga ham bo'linadiganlarini chiqaring (for + if).
+// let N40 = 30;
+// Javob:
+// for (let i = 1; i <= N40; i++) {
+//   if (i % 2 === 0 && i % 3 === 0) {
+//     console.log(i);
+//   }
+// }
+
+// 41-masala
+// Berilgan son mukammal son ekanligini tekshiring.
+// let son41 = 28;
+// Javob:
+// let sum41 = 0;
+// for (let i = 1; i < son41; i++) {
+//   if (son41 % i === 0) {
+//     sum41 += i;
+//   }
+// }
+// if (sum41 === son41) {
+//   console.log("Mukammal son");
+// } else {
+//   console.log("Mukammal son emas");
+// }
+
+// 42-masala
+// 1 dan 1000 gacha bo'lgan barcha tub sonlarni toping (for ichida for).
+// Javob:
+// for (let i = 2; i <= 1000; i++) {
+//   let tub = true;
+//   for (let j = 2; j < i; j++) {
+//     if (i % j === 0) {
+//       tub = false;
+//       break;
+//     }
+//   }
+//   if (tub) {
+//     console.log(i);
+//   }
+// }
+
+// 43-masala
+// Berilgan sonni ikkilik sanoq sistemasiga o'tkazing (while). Masalan: 13 -> 1101
+// let son43 = 13;
+// Javob:
+// let binary43 = "";
+// while (son43 > 0) {
+//   binary43 = (son43 % 2) + binary43;
+//   son43 = Math.floor(son43 / 2);
+// }
+// console.log(binary43);
---
+++ jsbek/while.js
@@ -0,0 +1,48 @@
+// let names = ["Saidjon", "Shohjahon", "Azamat", "Suhrob", "Jahongir"]
+// console.log(names[0])
+// console.log(names[2])
+// console.log(names[4])
+// console.log(names[3])
+
+// let massiv = new Array(4)
+// let massiv0 = new Array(4, 1, 2, 3, 5)
+// let massiv1 = new Array[]
+// console.log(massiv)
+// console.log(massiv0)
+// console.log(massiv1)
+// console.log(massiv.length)
+
+// --------------------------------------------
+// 1 - Massiv yaratish va chiqarish
+// --------------------------------------------
+// [1, 2, 3, 4, 5] massivini yarating va har bir elementni konsolga chiqaring
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// --------------------------------------------
+// 2 - Oxiriga element qo'shish
+// --------------------------------------------
+// [10, 20, 30] massivining oxiriga 40 ni qo'shing va natijani chiqaring
+
+// let arr = [10, 20, 30];
+
+// for (let i of arr) {
+//   console.log(i)
+// }
+
+// console.log(arr);
+
+// --------------------------------------------
+// 3 - Oxirgi elementni o'chirish
+// --------------------------------------------
+// [5, 10, 15, 20] massivining oxirgi elementini o'chiring va natijani chiqaring
+
+// let arr = [5, 10, 15, 20];
+
+// arr.pop();
+
+// console.log(arr)
