4e9d819d4b116d97b2c1eccef1579fb46cea3b5c
ASTA990 <grandsaidjon@gmail.com>
2026-07-03 16:22:33 +0500

    add

---
+++ jsbek/arr.js
@@ -0,0 +1,30 @@
+let arr = [23, 54, 76, 54, 346, 234, 213, 12, 31, 3, 4, 5, 345, 3, 4, 53, 54];
+
+// arr.pop()
+
+// arr.push(1, 2, 3, 4, 5)
+
+// arr.unshift(1, 2, 3, 4, 5)
+
+// arr.shift()
+
+// arr.splice(1, 15, 7);
+
+// arr.splice(1, 15);
+
+// arr.splice(1);
+
+// let indeksiniQaytarish = arr.indexOf(54)
+
+// let indeksiniQaytarish1 = arr.indexOf(545)
+
+// console.log(indeksiniQaytarish, indeksiniQaytarish1)
+
+// let massiv = arr.lastIndexOf(54)
+
+// console.log(massiv)
+
+// let findMetod = arr.find(i => i > 300)
+
+// console.log(findMeto)
+                    
---
+++ jsbek/methods.js
@@ -0,0 +1,18 @@
+// let arr = ["olma", "anor"];
+// arr.push("uzum");
+// console.log(arr); 
+
+// let arr = ["olma", "anor", "uzum"];
+// let oxirgi = arr.pop();
+// console.log(oxirgi);
+// console.log(arr);
+
+// let arr = ["anor", "uzum"];
+// arr.unshift("olma");
+// console.log(arr);
+
+// let arr = ["olma", "anor", "uzum"];
+// let birinchi = arr.shift();
+// console.log(birinchi);
+// console.log(arr);
+
